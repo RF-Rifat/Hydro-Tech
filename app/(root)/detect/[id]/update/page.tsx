0417f66bce33bc19783c8b5ed87a3c2bb6d8d08a
RF-Rifat <rfrifat6344@gmail.com>
2024-05-19 10:32:41 +0600

    button color logo updated

--- app/(root)/detect/[id]/update/page.tsx
+++ app/(root)/detect/[id]/update/page.tsx
@@ -21,12 +21,7 @@ const UpdateEvent = async ({ params: { id } }: UpdateEventProps) => {
       </section>
 
       <div className="wrapper my-8">
-        <EventForm 
-          type="Update" 
-          event={event} 
-          eventId={event._id} 
-          userId={userId} 
-        />
+        <EventForm />
       </div>
     </>
   )
